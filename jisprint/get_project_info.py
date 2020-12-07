cd7f363a246fe3aec7ca57418e6281d15f8373f8
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2020-12-07 16:52:53 +0100

    Add get-project-info tool

--- jisprint/get_project_info.py
+++ jisprint/get_project_info.py
@@ -1,6 +1,8 @@
 import argparse
 import logging
 from jira import JIRA
+from datetime import datetime
+
 from .util import str2bool, create_summary
 
 log = logging.getLogger("tool")
@@ -10,7 +12,9 @@ def main():
     logging.basicConfig()
 
     parser = argparse.ArgumentParser(description="JIRA epic info tool")
-    parser.add_argument("epic", type=str, help="the epic id.")
+    parser.add_argument("project", type=str, help="the project name.")
+    parser.add_argument("--since", type=str, help="a minimum date like 2020.12.07.")
+    parser.add_argument("--until", type=str, help="a maximum date like 2020.12.07.")
     parser.add_argument("--urls", type=str2bool, nargs="?", const=True, help="Show urls", default=False)
     parser.add_argument("--debug", type=str2bool, nargs="?", const=True, default=False)
 
@@ -21,13 +25,20 @@ def main():
         log.setLevel(logging.DEBUG)
         log.debug("Debug mode enabled")
 
-    epic_id = args.epic
+    project_id = args.project
 
-    jql = f'issuetype in (Epic, Story, Bug, Task, subTaskIssueTypes()) AND "epic link" = {epic_id}'
+    start_date = None
+    if args.since:
+        start_date = datetime.strptime(args.since, '%Y.%m.%d')
+    end_date = None
+    if args.until:
+        end_date = datetime.strptime(args.until, '%Y.%m.%d')
+
+    jql = f'issuetype in (Epic, Story, Bug, Task, subTaskIssueTypes()) AND project = {project_id}'
 
     jiraobj = JIRA({"server": "https://jira.camptocamp.com"})
     results = jiraobj.search_issues(jql)
-    create_summary(jiraobj, results, None, None, args.urls)
+    create_summary(jiraobj, results, start_date, end_date, args.urls)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     entry_points={"console_scripts": [
         "get-single-sprint = jisprint.get_single_sprint:main",
         "get-board-sprints = jisprint.get_board_sprints:main",
-        "get-epic-info = jisprint.get_epic_info:main"
+        "get-epic-info = jisprint.get_epic_info:main",
+        "get-project-info = jisprint.get_project_info:main"
     ]},
 )
